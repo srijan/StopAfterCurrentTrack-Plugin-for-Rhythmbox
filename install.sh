1e17e27e995095cc3656521ab93ca592d431aab6
Srijan Choudhary <srijan4@gmail.com>
2012-08-21 01:01:21 +0530

    Installation script update
    Uses ${HOME}/ instead of /home/${USER}/

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ GLIB_SCHEME="org.gnome.rhythmbox.plugins.lastfm_queue.gschema.xml"
 GLIB_DIR="/usr/share/glib-2.0/schemas/"
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/StopAfterCurrentTrack/"  
+PLUGIN_PATH="${HOME}/.local/share/rhythmbox/plugins/StopAfterCurrentTrack/"
 
 #build the dirs
 mkdir -p $PLUGIN_PATH
